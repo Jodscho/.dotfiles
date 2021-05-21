fbcb597769f3bcea7f3f39456ab5a50aa2339308
Jodscho <Jonathan2008@gmx.de>
2021-05-21 11:32:20 +0200

    improve change_theme scripts, style changes

--- scripts/change_theme.sh
+++ scripts/change_theme.sh
@@ -37,4 +37,25 @@ for col in "${colors[@]}"; do
   ((i++))
 done
 
+# dunst
+dunst_frame="frame_color = \"${colors[1]}\""
+dunst_background="background = \"${colors[0]}\""
+dunst_foreground="foreground = \"${colors[15]}\""
+sed -i "s/frame_color = .*/${dunst_frame}/g" "$HOME/.config/dunst/dunstrc"
+sed -i "s/background = .*/${dunst_background}/g" "$HOME/.config/dunst/dunstrc"
+sed -i "s/foreground = .*/${dunst_foreground}/g" "$HOME/.config/dunst/dunstrc"
+
+# zathura
+zathura_fg="set default-fg \"${colors[15]}\""
+zathura_fg="set default-fg \"${colors[15]}\""
+sed -i "s/set default-fg .*/${zathura_fg}/g" "$HOME/.config/zathura/zathurarc"
+
+sed -i "s/set completion-bg .*/set completion-bg \"${colors[0]}\"/g" "$HOME/.config/zathura/zathurarc"
+sed -i "s/set completion-bg .*/set completion-fg \"${colors[7]}\"/g" "$HOME/.config/zathura/zathurarc"
+sed -i "s/set completion-highlight-bg .*/set completion-highlight-bg \"${colors[2]}\"/g" "$HOME/.config/zathura/zathurarc"
+sed -i "s/set completion-highlight-fg .*/set completion-highlight-fg \"${colors[0]}\"/g" "$HOME/.config/zathura/zathurarc"
+sed -i "s/set inputbar-bg .*/set inputbar-bg \"${colors[0]}\"/g" "$HOME/.config/zathura/zathurarc"
+sed -i "s/set inputbar-fg .*/set inputbar-fg \"${colors[7]}\"/g" "$HOME/.config/zathura/zathurarc"
+
+# tabbed
 
